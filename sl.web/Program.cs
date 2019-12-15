f020dc9d707587c21717f757754ac3357e593ed1
Diyaz Yakubov <diyaz.yakubov@exsensio.com>
2019-12-15 22:55:44 +0300

    master | added sl.web project

---
+++ sl.web/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace sl.web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Title = "service-log";
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
