8723c3bc27f2696b11cecedd0ce3c359e1fd54d9
Diyaz Yakubov <diyaz.yakubov@exsensio.com>
2020-11-18 12:52:25 +0200

    adjusted program.cs

--- sl.web/Program.cs
+++ sl.web/Program.cs
@@ -1,24 +1,11 @@
 using System;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using sl.web;
 
-namespace sl.web
-{
-    public class Program
+Console.Title = "service-log";
+Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder =>
     {
-        public static void Main(string[] args)
-        {
-            Console.Title = "service-log";
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+        webBuilder.UseStartup<Startup>();
+    }).Build().Run();
